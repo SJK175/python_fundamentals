1f56df745cdc17065ef63c01f5c4a5527a5ad984
Subhajit Kundu <mrsubhajitkundu@gmail.com>
2021-05-01 19:01:53 +0530

    Update if_elif_else.py
    some more examples

--- if_elif_else.py
+++ if_elif_else.py
@@ -11,3 +11,25 @@ if good_credit_score :
 else:
     dp = price*0.2
 print(f"downpayment amount ${dp}")
+
+
+
+
+#approve the loan if he's our employee & income > 7lacks -else,
+#approve the lone if the yearly income is more than 10lacks
+#and credit score is more than 650
+#and no criminal record.
+
+our_employee = False
+income = 1500000
+credit_score = 675
+cri_rec = False
+
+if our_employee and income > 700000:
+    print("loan approved")
+elif not our_employee and income >1000000 and credit_score >650 and not cri_rec:
+    print("loan approved")
+else :
+    print("loan not approved")
+
+
