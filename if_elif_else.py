64f8121cd14fc7ec11731988dc94686892f7622c
Subhajit Kundu <mrsubhajitkundu@gmail.com>
2021-05-01 18:37:30 +0530

    Create if_elif_else.py
    learning if_elif logic in python

---
+++ if_elif_else.py
@@ -0,0 +1,13 @@
+
+#price of a house is $1M. If buyer has good credit score
+#he needs to put down 10%.otherwise they need to
+#put down 20%. print the down-payment amount.
+
+price = 1000000
+good_credit_score = False
+
+if good_credit_score :
+    dp = price*0.1
+else:
+    dp = price*0.2
+print(f"downpayment amount ${dp}")
