9de759dd12aa91246e80387f0dda583ee5f135a5
Subhajit Kundu <mrsubhajitkundu@gmail.com>
2021-05-05 20:56:59 +0530

    Update loops.py
    another example

--- loops.py
+++ loops.py
@@ -19,12 +19,20 @@ else :
 # for loop
 # calculate total value of cart items & print the
 # total value after giving 10% discount
-
 cart = [100, 150, 200, 50]
 bill = 0
 discount = 0.1
 for item in cart :
     bill = bill + item
 print(f"total bill : {bill-bill*discount}" )
+
+
+# write a program to find the largest no of a list.
+my_list = [12,15,100,76,9,104]
+max_item = my_list[0]
+for i in my_list:
+    if i> max_item:
+        max_item = i
+print(max_item)
 
 
