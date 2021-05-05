3a633c8e0306c1113754b31ff75feaa673085116
Subhajit Kundu <mrsubhajitkundu@gmail.com>
2021-05-05 21:03:32 +0530

    Update loops.py
    2D list

--- loops.py
+++ loops.py
@@ -36,3 +36,10 @@ for i in my_list:
 print(max_item)
 
 
+# print each item of the matrix
+matrix = [[1,2,3],
+          [4,5,6],
+          [7,8,9]]
+for row in matrix:
+    for item in row:
+        print(item)
