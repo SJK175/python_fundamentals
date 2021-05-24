adbbb417a8f8d3435dd0e915a9a802c75dbe759e
Subhajit Kundu <mrsubhajitkundu@gmail.com>
2021-05-24 16:21:19 +0530

    Update emailid_generaion.py
    this version also checks the email id with reverse name i.e if the input is subhajit kundu then it checks : subhajit.kundu@linde.com and kundu.subhajit@linde.com

--- emailid_generaion.py
+++ emailid_generaion.py
@@ -1,25 +1,31 @@
 #this program should convert a name to a valid email address.
-
 firstname = input("firstname :")
 lastname = input("lastname :")
 domain = "linde.com"
+
 emaillist = ["subhajit.kundu@linde.com"
     ,"barry.wood@linde.com"
     ,"biswajit.ghosh@linde.com"
     ,"therina.foley@linde.com"
-    ,"jaydeep.basu@linde.com"]
+    ,"jaydeep.basu@linde.com"
+    ,"kundu.subhajit@linde.com"]
 
-email = firstname+"."+lastname+"@"+domain
+email_1 = firstname.strip()+"."+lastname.strip()+"@"+domain
+email_2 = lastname.strip()+"."+firstname.strip()+"@"+domain
 
 counter = 0
 for e in emaillist:
-    if e == email:
+    if e == email_1:
         counter = counter+1
+        for e1 in emaillist:
+            if e1 == email_2:
+                counter = counter+1
     else:
         counter = counter+0
 
-#print(counter)
-if counter == 0:
-    print(f"suggested email address is : {email}")
-else:
-    print("this email-id already exists")
+if counter == 2:
+    print("can't suggest any email-id")
+elif counter == 1:
+    print(f"suggested email is {email_2}")
+elif counter == 0:
+    print(f"suggested email is {email_1}")
